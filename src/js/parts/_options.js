7971b2fb535ebe877cee7a8d67270419628aa97c
John Adams <john@twothink.co>
2019-06-02 23:46:41 -0700

    remove some bits

--- src/js/parts/_options.js
+++ src/js/parts/_options.js
@@ -21,7 +21,7 @@ const options = {
 
         cookieAlert:
             `<span class="nk-cookie-alert-close"><span class="nk-icon-close"></span></span>
-            Cookie alert are ready to use. You can simply change content inside this alert or disable it in javascript theme options. <a href="#">Cookies policy</a>.
+            This site uses cookies to track usage and logs a limited amount of user data. By clicking confirm, you consent to the use of cookies and this tracking. <a href="#">Cookie policy</a>.
             <div class="nk-gap"></div>
             <span class="nk-cookie-alert-confirm nk-btn link-effect-4 nk-btn-bg-white nk-btn-color-dark-1">Confirm</span>`,
 
